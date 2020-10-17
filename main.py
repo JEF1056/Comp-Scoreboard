07f1b82105478520b540608ad0c0be7cf1a58b91
Jess Edmund Fan <jessefan2002@gmail.com>
2020-10-17 14:29:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -163,6 +163,10 @@ def upload_accept():
             errors.append("Invalid file")
     else:
         errors.append("ya ain't part of no team!")
+    t_scores=teams.find_one(team=team["team"])
+    for ctf in t_scores:
+    if ctf != "id" and ctf != team["team"]:
+        scores[ctf] = t_scores[ctf]
     return render_template("upload.html", ctfs=config, user=str(discord_id), team=team["team"], scores=scores, errors=errors)
 
 @app.route("/admin")
