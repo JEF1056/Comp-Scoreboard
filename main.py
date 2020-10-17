84778322f0d3025f369c3fb731eb497ecbeacc63
Jess Edmund Fan <jessefan2002@gmail.com>
2020-10-17 15:15:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def scores():
     all_rows=teams.find(id={'>=': 0})
     teams=[]
     team_totals=[]
-    unscaled_team_scores=[0]*(len(all_rows))
+    unscaled_team_scores=[0]*(len(list(all_rows)))
     scaled_ctfs=[]
     ctfs={}
     print(unscaled_team_scores)
