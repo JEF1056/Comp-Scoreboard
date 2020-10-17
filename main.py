07cba1b42e82b90b7b0d77d651a2c6f45932f404
Jess Edmund Fan <jessefan2002@gmail.com>
2020-10-17 15:32:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -64,10 +64,7 @@ def scores():
     all_rows=teams.find(id={'>=': 0})
     teams=[]
     team_totals=[]
-    print(all_rows)
-    print(len(list(all_rows)))
-    print(all_rows)
-    unscaled_team_scores=[0]*teams.count(all_rows)
+    unscaled_team_scores=[0]*2
     scaled_ctfs=[]
     ctfs={}
     counter=0
@@ -82,7 +79,7 @@ def scores():
                     ctfs[ctf].append(row[ctf])
                 except:
                     ctfs[ctf] = [row[ctf]]
-                unscaled_team_scores[counter]+=row[ctf]
+                #unscaled_team_scores[counter]+=row[ctf]
         counter+=1
     color = hex_to_rgb("#"+str(hex(np.random.randint(0,16777215)))[2:])
     ctf_fixed=[]
